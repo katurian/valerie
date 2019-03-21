d001213684fbb5fefed41d2b12172dfb68fcf01c
Kate Kulinski <35544223+katurian@users.noreply.github.com>
2019-03-20 20:05:39 -0400

    Update therapy.py

--- therapy.py
+++ therapy.py
@@ -4,9 +4,30 @@ from discord.ext.commands import Bot
 from discord.ext import commands
 import platform
 import feedparser
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
 
 
-client = Bot(description="cbt bot by Kat", command_prefix="!", pm_help = False)
+bot = ChatBot(
+	'Rachel',
+	storage_adapter='chatterbot.storage.SQLStorageAdapter',
+	database_uri='sqlite:///database.sqlite3'
+)
+
+trainer = ListTrainer(bot)
+
+trainer.train([
+	'How are you?',
+	'I am good.',
+	'That is good to hear.',
+	'Thank you',
+	'You are welcome.',
+])
+
+
+
+client = Bot(description="cbt bot by Kat", command_prefix="", pm_help = False)
+
 
 @client.event
 async def on_ready():
@@ -19,7 +40,11 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('$'):
-      await client.say("kate")
+	if message.content.startswith('Rachel') or message.content.startswith('rachel') or message.content.startswith('rach'):
+		await client.send_message(message.channel, 'Hello')
+		msg = await client.wait_for_message(author=message.author)
+		response = chatbot.get_response('I would like to book a flight.')
+		await client.send_message(message.channel, response)
 
-client.run('')
+client.run('NTU4MDYxNTU2ODQwMDA1NjUy.D3RXsA.tdLDmVflIcgeg7HZnDEr1oDtjz0')
+
