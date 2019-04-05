d9b038f42f1a368ccf24f1632d71f25474ee6563
Kate Kulinski <35544223+katurian@users.noreply.github.com>
2019-04-05 00:54:28 -0400

    Update val.py

--- val.py
+++ val.py
@@ -44,4 +44,4 @@ async def on_message(message):
 		await client.send_message(message.channel, response)
 
 
-client.run('CLIENT-TOKEN')
+client.run('NTYxMjk0NTA0NjExODA3MjU0.XKbfbw.S75QxcehvpYyqmHwNBH0bDI6z8o')
